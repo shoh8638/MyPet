572fff075f6c75f653c921d603132666b808917b
shoh <shoh8638@gmail.com>
2023-05-12 17:07:10 +0900

    FEAT: 1. category-WriteVC 관련하여 gesture event 설정

--- MyPet/VC/Main/WriteVC.swift
+++ MyPet/VC/Main/WriteVC.swift
@@ -12,15 +12,19 @@ class WriteVC: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var downloadImg: UIView!
+    @IBOutlet weak var miniImg: UIImageView!
+    @IBOutlet weak var imgName: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var categoryList: [String]?
     var previousButton: CategoryCell?
-    var btnTag: Int?
+    var imginfo: [UIImagePickerController.InfoKey : Any]! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configure()
-
+        self.fillPlaceHolderAndAccessory()
     }
     
     private func configure() {
@@ -28,7 +32,7 @@ class WriteVC: UIViewController {
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
     }
-
+    
     @IBAction func closeTap(_ sender: Any) {
         print("CloseTap")
     }
@@ -36,7 +40,7 @@ class WriteVC: UIViewController {
     @IBAction func checkCloseTap(_ sender: Any) {
         print("checkTap")
     }
-
+    
     @IBAction func saveTap(_ sender: Any) {
         print("saveTap")
     }
@@ -52,7 +56,7 @@ extension WriteVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayou
         cell.titleLabel.text = self.categoryList![indexPath.item]
         cell.backView.backgroundColor = .orange
         cell.backView.layer.cornerRadius = 10
-       return cell
+        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -63,7 +67,7 @@ extension WriteVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayou
         guard let cell = collectionView.cellForItem(at: indexPath) as? CategoryCell else { return }
         cell.backView.backgroundColor = .lightGray
         cell.backView.layer.cornerRadius = 10
-
+        
         self.previousButton = cell
         print(self.categoryList![indexPath.item])
     }
@@ -74,19 +78,53 @@ extension WriteVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayou
         return CGSize(width: width, height: height)
     }
 }
+extension WriteVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else  { return }
+        guard let data = image.jpegData(compressionQuality: 0.8) else { return }
+        guard let resizeImg = UIImage(data: data) else { return }
+        self.imginfo = info
+        guard let imageURL = info[.imageURL] as? URL else { return }
+        let imageName = imageURL.lastPathComponent
+        self.addImageView(image: resizeImg, imgName: imageName)
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func openGallery(_ gesture: UITapGestureRecognizer) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func addImageView(image: UIImage, imgName: String) {
+        self.miniImg.image = image
+        self.miniImg.clipsToBounds = true
+        self.miniImg.isUserInteractionEnabled = true
+        self.imgName.text = imgName
+    }
+}
 
 extension WriteVC: UITextFieldDelegate {
     func fillPlaceHolderAndAccessory() {
         self.titleField.delegate = self
         self.textField.delegate = self
+        self.scrollView.isUserInteractionEnabled = true
+        
+        let dismissSelected = UITapGestureRecognizer(target: self, action: #selector(self.dismissSelectBox))
+        dismissSelected.cancelsTouchesInView = false
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(self.openGallery(_:)))
+        
+        self.downloadImg.addGestureRecognizer(gesture)
+        self.view.addGestureRecognizer(dismissSelected)
     }
     
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        if textField == self.titleField {
-            return true
-        } else if textField == self.textField {
-            return true
-        }
-        return false
+    @objc func dismissSelectBox() {
+        self.view.endEditing(true)
     }
 }
